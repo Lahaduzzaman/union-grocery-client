14beef453c44b9ff43b9fd00860182ff4c7c73e5
Lahaduzzaman <criclahad.2@gmail.com>
2021-04-30 01:44:58 +0600

    okay

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse justify-content-end font-weight-bold" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auton mb-2 mb-lg-0">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                             </li>
